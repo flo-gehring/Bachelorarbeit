b76aa4f257be2df88a4bba51c9811cdcc7393e3d
Florian Gehring <florian.gehring@pm.me>
2018-10-16 11:35:06 +0200

    Tracking Class

--- tracking.cpp
+++ tracking.cpp
@@ -4,3 +4,26 @@
 
 #include "tracking.h"
 
+
+void CustomMultiTracker::initialize_darknet(Mat &frame) {
+
+    darknetDetector.detect_and_display(frame);
+
+
+
+
+    for(auto it = darknetDetector.found.begin();
+    it != darknetDetector.found.end(); ++it){
+        algorithms.push_back(TrackerKCF::create());
+        objects.push_back((*it).rect);
+    }
+    multiTracker.add(algorithms, frame, objects);
+
+}
+
+void CustomMultiTracker::update(Mat &frame) {
+    multiTracker.update(frame);
+    for(unsigned i=0;i<multiTracker.getObjects().size();i++)
+        rectangle( frame, multiTracker.getObjects()[i], Scalar( 255, 0, 0 ), 2, 1 );
+
+}
